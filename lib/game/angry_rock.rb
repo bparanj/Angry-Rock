69d8bc1e8c99066f6db3919f893fcb72ee92bee3
Bala Paranj <bparanj@gmail.com>
2012-05-13 08:03:01 -0700

    play method that violated Command Query Separation is now gone.

--- lib/game/angry_rock.rb
+++ lib/game/angry_rock.rb
@@ -24,14 +24,8 @@ module Game
     # Problem : Is this method is a command and a query?
     # It is ambiguous because play seems to be a name of a command and 
     # it is returning the winning AngryRock object
-    def play(other)
-      if self > other
-        self
-      elsif other > self
-        other
-      end
-    end
-
+    # play method that violated Command Query Separation is now gone.
+    # This is a query method 
     def winner(other)
       if self > other
         self
--- spec/game/angry_rock_spec.rb
+++ spec/game/angry_rock_spec.rb
@@ -24,20 +24,19 @@ module Game
    it "picks rock as the winner over scissors " do
      choice_1 = AngryRock.new(:rock)
      choice_2 = AngryRock.new(:scissors)
-     winner = choice_1.play(choice_2)
-     result = winner.move
+     play = Play.new(choice_1, choice_2)
      
-     result.should == "rock"      
+     play.should have_winner
+     play.winning_move.should == "rock"          
    end
    
-   xit "results in a tie when the same choice is made by both players : rock, paper or scissors" do
+   it "results in a tie when the same choice is made by both players : rock, paper or scissors" do
      data_driven_spec([:rock, :paper, :scissors]) do |choice|
        choice_1 = AngryRock.new(choice)
        choice_2 = AngryRock.new(choice)
-       winner = choice_1.play(choice_2)
-       result = winner.move
-       
-       result.should == "Both players selected : #{choice}. It's a TIE!"      
+       play = Play.new(choice_1, choice_2)
+
+       play.should_not have_winner
      end     
    end   
    
